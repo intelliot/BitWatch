997f844351b2c7ce175a901e1ba35b7f49234765
Elliot Lee <elliot.lee@getbraintree.com>
2015-04-11 11:53:40 -0700

    Implement priceLabel and refreshTapped. Add Fonts to BitWatch WatchKit App target. Update CFBundleIdentifier

--- BitWatch WatchKit Extension/InterfaceController.swift
+++ BitWatch WatchKit Extension/InterfaceController.swift
@@ -1,26 +1,47 @@
-//
-//  InterfaceController.swift
-//  BitWatch WatchKit Extension
-//
-//  Created by Elliot Lee on 4/11/15.
-//  Copyright (c) 2015 Razeware LLC. All rights reserved.
-//
-
 import WatchKit
 import Foundation
-
+import BitWatchKit
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var priceLabel: WKInterfaceLabel!
+    let tracker = Tracker()
+    var updating = false
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        updatePrice(tracker.cachedPrice())
     }
 
+    // Similar to viewWillAppear on iOS
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        update()
+    }
+    
+    @IBAction func refreshTapped() {
+        update()
+    }
+    
+    private func updatePrice(price: NSNumber) {
+        priceLabel.setText(Tracker.priceFormatter.stringFromNumber(price))
+    }
+    
+    private func update() {
+        if !updating {
+            updating = true
+            let originalPrice = tracker.cachedPrice()
+            tracker.requestPrice { (price, error) -> () in
+                if error == nil {
+                    self.updatePrice(price!)
+                }
+                self.updating = false
+            }
+        }
     }
 
     override func didDeactivate() {
