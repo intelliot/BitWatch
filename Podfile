df3ffe34e821b7d1381132e5a1fa8fb99eee7bb3
Elliot Lee <elliot.lee@getbraintree.com>
2015-04-12 20:08:19 -0700

    Create Podfile including pod "coinbase-official" and pod install
    - CocoaPods 0.36.3

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'BitWatch' do
+
+end
+
+target 'BitWatchKit' do
+  pod "coinbase-official"
+end
+
+target 'BitWatch WatchKit Extension' do
+
+end
+
+target 'BitWatch WatchKit App' do
+
+end
+
+target 'BitWatch Tests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-BitWatchKit/Pods-BitWatchKit-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// coinbase-official
+#define COCOAPODS_POD_AVAILABLE_coinbase_official
+#define COCOAPODS_VERSION_MAJOR_coinbase_official 2
+#define COCOAPODS_VERSION_MINOR_coinbase_official 1
+#define COCOAPODS_VERSION_PATCH_coinbase_official 2
+
+// coinbase-official/OAuth
+#define COCOAPODS_POD_AVAILABLE_coinbase_official_OAuth
+#define COCOAPODS_VERSION_MAJOR_coinbase_official_OAuth 2
+#define COCOAPODS_VERSION_MINOR_coinbase_official_OAuth 1
+#define COCOAPODS_VERSION_PATCH_coinbase_official_OAuth 2
+
+// coinbase-official/client
+#define COCOAPODS_POD_AVAILABLE_coinbase_official_client
+#define COCOAPODS_VERSION_MAJOR_coinbase_official_client 2
+#define COCOAPODS_VERSION_MINOR_coinbase_official_client 1
+#define COCOAPODS_VERSION_PATCH_coinbase_official_client 2
+
